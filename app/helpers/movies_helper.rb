901274151885826d9874ee8a3ab03764ebeaf938
EC2 Default User <ec2-user@ip-172-31-29-218.us-east-2.compute.internal>
2018-03-07 04:29:05 +0000

    part 1 complete

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,11 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  def selected?(field)
+    if(params[:sort].to_s == field)
+      return 'hilite'
+    else
+      return nil
+    end
+  end
 end
